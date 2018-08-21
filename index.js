594baef7dd7e8ce5cebab34629a91919293deb1a
Roman Khudyakov <roma.khudyakov@gmail.com>
2018-08-22 02:33:22 +0300

    refactoring search, adding focus, blur events, fix copy-paste, fix parsing, adding scss

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 
 module.exports = {
   name: 'ember-intl-phone-input',
-  // addon index.js
-  isDevelopingAddon() {
-    return true;
-  }
+
+  included() {
+    let findHost = this._findHost;
+    let app = findHost.call(this);
+
+    if (!app.__emberIntlPhoneInputIncluded) {
+
+      let hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
+      let hasLess = !!app.registry.availablePlugins['ember-cli-less'];
+
+      if (!hasSass && !hasLess) {
+        app.import(`vendor/ember-intl-phone-input.css`);
+      }
+
+      app.__emberIntlPhoneInputIncluded = true;
+    }
+  },
 };
